49696cbd1031e2030fd6049b783855f55ed85fdc
joelYing <39625260+joelYing@users.noreply.github.com>
2018-08-29 08:23:36 +0000

    Update lagou.py

--- lagou.py
+++ lagou.py
@@ -27,16 +27,16 @@ class Spider():
         self.url = 'https://www.lagou.com/jobs/positionAjax.json?needAddtionalResult=false'
         self.headers = {
             'Connection': 'keep-alive',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
+            'User-Agent': 'your-user-agent',
             # 'Host': 'www.lagou.com',
             # 'Origin': 'https://www.lagou.com',
             'Referer': 'https://www.lagou.com/jobs/list_%E7%88%AC%E8%99%AB%E5%B7%A5%E7%A8%8B%E5%B8%88?labelWords=&fromSearch=true&suginput=',
             'X-Requested-With': 'XMLHttpRequest'
         }
         self.cookies = {
-            'Cookie': '_ga=GA1.2.186238179.1522124084; user_trace_token=20180327121444-61634056-3175-11e8-b64a-5254005c3644; LGUID=20180327121444-616343dd-3175-11e8-b64a-5254005c3644; index_location_city=%E5%85%A8%E5%9B%BD; _gid=GA1.2.333845926.1523191963; showExpriedIndex=1; showExpriedCompanyHome=1; showExpriedMyPublish=1; hasDeliver=0; JSESSIONID=ABAAABAAAIAACBI545930EC9077BFB71ECD5FD34365CB66; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1522225335,1522317804,1523191963,1523265610; _gat=1; LGSID=20180409210723-f1d1af71-3bf6-11e8-b7e1-525400f775ce; PRE_UTM=; PRE_HOST=; PRE_SITE=https%3A%2F%2Fwww.lagou.com%2Fjobs%2Flist_%25E7%2588%25AC%25E8%2599%25AB%25E5%25B7%25A5%25E7%25A8%258B%25E5%25B8%2588%3FlabelWords%3D%26fromSearch%3Dtrue%26suginput%3D; PRE_LAND=https%3A%2F%2Fwww.lagou.com%2F; gate_login_token=""; X_HTTP_TOKEN=6cfc3271cbdf2db7a2389dfca7086f1f; LG_LOGIN_USER_ID=acb79242d9e74c3313a7f1945e11e306280dcd61206312d02f812b5d33480d50; _putrc=21AD074BCC431F46123F89F2B170EADC; login=true; unick=%E5%BA%94%E5%AE%B6%E4%B9%90; gate_login_token=329388605c33d8ef508ee676269b40661351a7862f8b5b56af312da6a992400c; LGRID=20180409210823-154d3a4d-3bf7-11e8-b742-5254005c3644; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1523279299; TG-TRACK-CODE=index_search; SEARCH_ID=20e6f6fb5efc46bca33eaa95a9255d35'}
+            'Cookie': 'cookie'}
         self.tool = Tool()
-        self.client = pymongo.MongoClient(host='localhost', port=27017)
+        self.client = pymongo.MongoClient(host='localhost', port=port)
         self.db = self.client['lagou']
 
     def getHtml(self, url, datas):
